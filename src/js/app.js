69666ef39b4e8637ba681c849c12a558c21edba6
Emilian Chiper <emilian.chiper.developer@gmail.com>
2024-09-14 18:13:48 +0300

    Create logic for toggling modal visibility

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// Code
+// Imports
+const bookContainerElement = document.querySelector('.book-container');
+const modalElement = document.querySelector('.modal');
+
+const modalBtns = [
+  document.querySelector('.btn-add-book'),
+  document.querySelector('.close-modal'),
+];
+
+const toggleModalVisibility = function () {
+  modalElement.classList.toggle('open');
+};
+
+const attachHandlers = function (arr) {
+  arr.map(el => {
+    el.addEventListener('click', toggleModalVisibility);
+  });
+};
+
+attachHandlers(modalBtns);

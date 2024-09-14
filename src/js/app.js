d2ac676cb6d1cff0a92c5e279018d552d82e8fde
Emilian Chiper <emilian.chiper.developer@gmail.com>
2024-09-14 21:59:52 +0300

    Create logic for submitting book to bookshelf and displaying book

--- src/js/app.js
+++ src/js/app.js
@@ -13,33 +13,49 @@ const toggleModalVisibility = function () {
   modalElement.classList.toggle('open');
 };
 
-// const bookExample = {
-//   bookTitle: 'The Hobbit',
-//   bookAuthor: 'J.R.R. Tolkien',
-//   bookPubYear: 1937,
-//   bookPages: 321,
-//   bookStatus: 'undread',
-// };
+const bookShelves = [];
 
 const displayBook = function (book) {
   const { bookTitle, bookAuthor, bookPubYear, bookPages, bookStatus } = book;
+
   const markdown = `
-  <div class="card card-book">
-    <h4>${bookTitle}</h4>
-    <p>Author: <em>${bookAuthor}</em></p>
-    <p>Published: <em>${bookPubYear}</em></p>
-    <p>Pages: <em>${bookPages}</em></p>
-    <div class="button-container">
-      <button class="btn btn-status read">${bookStatus.toUpperCase()}</button>
-      <button class="btn btn-delete-book">DELETE</button>
+    <div class="card card-book">
+      <h4>${bookTitle}</h4>
+      <p>Author: <em>${bookAuthor}</em></p>
+      <p>Published: <em>${bookPubYear}</em></p>
+      <p>Pages: <em>${bookPages}</em></p>
+      <div class="button-container">
+        <button class="btn btn-status read">${bookStatus.toUpperCase()}</button>
+        <button class="btn btn-delete-book">DELETE</button>
+      </div>
     </div>
-  </div>
-  `;
+    `;
 
   bookContainerElement.insertAdjacentHTML('beforeend', markdown);
 };
 
-// displayBook(bookExample);
+const insertBook = function (e) {
+  e.preventDefault();
+  const bookTitleElement = document.querySelector('#book-title');
+  const bookAuthorElement = document.querySelector('#book-author');
+  const bookPubYearElement = document.querySelector('#book-publish-year');
+  const bookPagesElement = document.querySelector('#book-pages');
+  const bookStatusElement = document.querySelector('#book-status');
+
+  bookShelves.push({
+    bookTitle: `${bookTitleElement.value}`,
+    bookAuthor: `${bookAuthorElement.value}`,
+    bookPubYear: `${bookPubYearElement.value}`,
+    bookPagesElement: `${bookPagesElement.value}`,
+    bookStatus: `${bookStatusElement.value}`,
+  });
+
+  displayBook(bookShelves[bookShelves.length - 1]);
+  console.log(bookShelves[bookShelves.length - 1]);
+};
+
+const submitBookBtn = document.querySelector('.btn-submit-book');
+submitBookBtn.addEventListener('click', insertBook);
 
 const attachHandlers = function (arr) {
   arr.map(el => {
